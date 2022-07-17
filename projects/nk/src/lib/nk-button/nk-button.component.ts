4755499765d8bd062ebfe760899d32407c61e6ee
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-07-17 18:42:44 -0300

    feat: adicionando componente de botão e tentando resolver o imoprt

--- projects/nk/src/lib/nk-button/nk-button.component.ts
+++ projects/nk/src/lib/nk-button/nk-button.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NkButtonComponent implements OnInit {
 
   @Input() label!: string;
+  @Input() type!: string;
 
   constructor() { }
 
